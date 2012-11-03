3cf6ddcdf9bd980d2d1d0f1dfa4f9b28664a709f
vladexologija <vladimir@p3rsonal.com>
2012-11-03 13:50:54 +0100

    form input and validation

---
+++ controller.js
@@ -0,0 +1,20 @@
+function Controller($scope) {
+    $scope.data= {};
+   
+    $scope.update = function(user) {
+      $scope.data= angular.copy(user);
+    };
+   
+    $scope.reset = function() {
+      $scope.user = angular.copy($scope.data);
+      
+    };
+   
+    $scope.isUnchanged = function(user) {
+      $scope.ret = angular.equals(user, $scope.data);
+      return $scope.ret;
+     
+    };
+   
+    $scope.reset();
+  }
